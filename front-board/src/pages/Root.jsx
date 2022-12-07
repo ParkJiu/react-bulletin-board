48ce6d6e9c382960e26fff443fb7b78ee931a4c8
Jiu <jiupark25@gmail.com>
2022-12-07 12:02:08 +0900

    add front-board folder

---
+++ front-board/src/pages/Root.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+export default function Root() {
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+      <Footer/>
+    </div>
+  );
+}
+
